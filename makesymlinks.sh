6311f137383a5f0bf952e37263cc7d8e9c680816
vyshark <vyshak675@gmail.com>
2017-03-12 08:29:05 +0530

    added case switches

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -24,8 +24,19 @@ echo "...done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
 for file in $files; do
+
+	case $file  in
+		roficonfig)
+			destination="~/.config/rofi/config"
+			;;
+
+
+		*)destination="~/.$file"
+			;;
+
+		esac    # --- end of case ---
 	    echo "Moving any existing dotfiles from ~ to $olddir"
-	        mv ~/.$file ~/dotfiles_old/
+	        mv $destination  ~/dotfiles_old/
 		    echo "Creating symlink to $file in home directory."
-		        ln -s $dir/$file ~/.$file
+		        ln -s $dir/$file $destination
 		done
